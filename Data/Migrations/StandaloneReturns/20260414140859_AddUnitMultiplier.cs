95ab75b8e3669ee3674123ae2806047f6d109e6a
Abdallah <abdullah.taha574@gmail.com>
2026-04-14 16:11:59 +0200

    feat: add unit multiplier field to database schema via migration

---
+++ Data/Migrations/StandaloneReturns/20260414140859_AddUnitMultiplier.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Data.Migrations.StandaloneReturns
+{
+    /// <inheritdoc />
+    public partial class AddUnitMultiplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Multiplier",
+                table: "ProductUnits",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Multiplier",
+                table: "ProductUnits");
+        }
+    }
+}
