a557c590fd70444b35efc1ac88af6f07c622c50d
Abdallah <abdullah.taha574@gmail.com>
2026-04-14 18:18:09 +0200

    feat: add migration to fix purchase returns table schema

---
+++ Data/Migrations/StandaloneReturns/20260414161449_FixPurchaseReturnsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Data.Migrations.StandaloneReturns
+{
+    /// <inheritdoc />
+    public partial class FixPurchaseReturnsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
