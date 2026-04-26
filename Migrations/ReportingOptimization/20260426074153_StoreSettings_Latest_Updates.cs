fb1872db0514d2d88a843ed0b9f575c2763b9354
Abdallah <abdullah.taha574@gmail.com>
2026-04-26 14:00:21 +0300

    feat: add database migration for store settings updates

---
+++ Migrations/ReportingOptimization/20260426074153_StoreSettings_Latest_Updates.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Migrations.ReportingOptimization
+{
+    /// <inheritdoc />
+    public partial class StoreSettings_Latest_Updates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
