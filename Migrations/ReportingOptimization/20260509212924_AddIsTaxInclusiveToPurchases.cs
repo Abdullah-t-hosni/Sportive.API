c4caafadf77031dc14bc7b74ca3769dcd300b038
Abdallah <abdullah.taha574@gmail.com>
2026-05-10 00:30:25 +0300

    update

---
+++ Migrations/ReportingOptimization/20260509212924_AddIsTaxInclusiveToPurchases.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Migrations.ReportingOptimization
+{
+    /// <inheritdoc />
+    public partial class AddIsTaxInclusiveToPurchases : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTaxInclusive",
+                table: "PurchaseInvoices",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTaxInclusive",
+                table: "PurchaseInvoiceItems",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TaxRate",
+                table: "PurchaseInvoiceItems",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTaxInclusive",
+                table: "PurchaseInvoices");
+
+            migrationBuilder.DropColumn(
+                name: "IsTaxInclusive",
+                table: "PurchaseInvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "TaxRate",
+                table: "PurchaseInvoiceItems");
+        }
+    }
+}
