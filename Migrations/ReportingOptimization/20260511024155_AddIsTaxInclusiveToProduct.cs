81dce8f3db0f42fd18929638e6394c85eddcef5f
Abdallah <abdullah.taha574@gmail.com>
2026-05-11 12:18:59 +0300

    fix: generate missing IsTaxInclusive migration to resolve production 500 error

---
+++ Migrations/ReportingOptimization/20260511024155_AddIsTaxInclusiveToProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Migrations.ReportingOptimization
+{
+    /// <inheritdoc />
+    public partial class AddIsTaxInclusiveToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTaxInclusive",
+                table: "Products",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTaxInclusive",
+                table: "Products");
+        }
+    }
+}
