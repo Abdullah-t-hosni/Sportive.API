cf45f51aec6cfca365aca19d4a7cbf5ccf5b8999
Abdallah <abdullah.taha574@gmail.com>
2026-05-25 15:55:29 +0300

    chore: cleanup temporary sync endpoint

--- Controllers/Orders/OrdersController.cs
+++ Controllers/Orders/OrdersController.cs
@@ -40,14 +40,6 @@ public class OrdersController : ControllerBase
         _translator   = translator;
     }
 
-    [HttpGet("temp-sync")]
-    [AllowAnonymous]
-    public async Task<IActionResult> TempSync([FromServices] IAccountingService accounting)
-    {
-        await accounting.SyncEntityBalancesAsync();
-        return Ok("Sync Completed");
-    }
-
     [HttpGet]
     [RequirePermission(ModuleKeys.Orders)]
     [AllowPosAccess]
