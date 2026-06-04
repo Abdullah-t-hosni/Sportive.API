d50e71224af652c0922a776415c3315e2c777959
Abdallah <abdullah.taha574@gmail.com>
2026-06-04 22:25:14 +0300

    feat: add migration to remove employee fixed deduction field

---
+++ Migrations/ReportingOptimization/20260604192055_RemoveEmployeeFixedDeduction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Migrations.ReportingOptimization
+{
+    /// <inheritdoc />
+    public partial class RemoveEmployeeFixedDeduction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FixedDeduction",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "FixedDeduction",
+                table: "Employees",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
