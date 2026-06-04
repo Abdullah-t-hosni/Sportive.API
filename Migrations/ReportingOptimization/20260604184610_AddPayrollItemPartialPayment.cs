78c3d715a68e79f9d42aa6165ef6470ffb695331
Abdallah <abdullah.taha574@gmail.com>
2026-06-04 21:53:45 +0300

    feat: add partial payment support to payroll items via new migration

---
+++ Migrations/ReportingOptimization/20260604184610_AddPayrollItemPartialPayment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportive.API.Migrations.ReportingOptimization
+{
+    /// <inheritdoc />
+    public partial class AddPayrollItemPartialPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "PayrollItems",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaidAt",
+                table: "PayrollItems",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentJournalEntryId",
+                table: "PayrollItems",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "PayrollItems");
+
+            migrationBuilder.DropColumn(
+                name: "PaidAt",
+                table: "PayrollItems");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentJournalEntryId",
+                table: "PayrollItems");
+        }
+    }
+}
