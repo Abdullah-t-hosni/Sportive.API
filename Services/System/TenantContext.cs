6e926995a376496686ceeea6c00352dc3378818a
Abdallah <abdullah.taha574@gmail.com>
2026-06-23 03:26:22 +0300

    feat: implement TenantContext service using AsyncLocal to manage per-request tenant state

--- Services/System/TenantContext.cs
+++ Services/System/TenantContext.cs
@@ -1,11 +1,18 @@
 using Sportive.API.Interfaces;
 using Sportive.API.Models;
+using System.Threading;
 
 namespace Sportive.API.Services;
 
 public class TenantContext : ITenantContext
 {
-    public Tenant? CurrentTenant { get; private set; }
+    private static readonly AsyncLocal<Tenant?> _currentTenant = new();
+
+    public Tenant? CurrentTenant 
+    { 
+        get => _currentTenant.Value; 
+        private set => _currentTenant.Value = value; 
+    }
 
     public void SetTenant(Tenant tenant)
     {
