0e9623835baf010f44053ae51fd9145165632130
Abdallah <abdullah.taha574@gmail.com>
2026-06-28 02:04:02 +0300

    feat: add Brands table with parent hierarchy, update Category schemas, and implement InventoryAudit system

--- Migrations/20260331014903_AddParentToBrandAndRefactorProductBrand.cs
+++ Migrations/20260331014903_AddParentToBrandAndRefactorProductBrand.cs
@@ -14,8 +14,11 @@ namespace Sportive.API.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
-
+            migrationBuilder.AddColumn<int>(
+                name: "BrandId",
+                table: "Products",
+                type: "int",
+                nullable: true);
 
             migrationBuilder.AlterColumn<string>(
                 name: "NameEn",
